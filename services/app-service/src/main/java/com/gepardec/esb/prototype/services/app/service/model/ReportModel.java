3312f6fb19675ad069b99697a1be699f2936e13d
Thomas Herzog <herzog.thomas81@gmail.com>
2018-06-10 17:38:44 +0200

    - Introduced lombok

--- services/app-service/src/main/java/com/gepardec/esb/prototype/services/app/service/model/ReportModel.java
+++ services/app-service/src/main/java/com/gepardec/esb/prototype/services/app/service/model/ReportModel.java
@@ -1,8 +1,24 @@
 package com.gepardec.esb.prototype.services.app.service.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
 /**
  * @author Thomas Herzog <herzog.thomas81@gmail.com>
  * @since 06/08/18
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class ReportModel {
+
+    @JsonProperty("name")
+    @ApiModelProperty(value = "The report name", readOnly = true)
+    private String name;
 }
